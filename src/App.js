84e05db96bf02e9fba70e9bf8416f0f611a250df
Bo Latt <bolattny@gmail.com>
2022-10-16 15:39:57 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [cocktails, setCocktails ] = useState(null)
+  
+ function getData() { 
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+      'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://the-cocktail-db.p.rapidapi.com/popular.php', options)
+    .then(response => response.json())
+    .then(response => { 
+      console.log(response)
+      setCocktails(response.drinks)
+    })
+    .catch(err => console.error(err));
+ }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getData}>get drinks</button>
+      <ul>
+      {
+        cocktails && ( 
+          cocktails.map(cocktail => (
+            <li key={cocktail.idDrink}>{cocktail.strDrink}</li>
+          ))
+        ) 
+      }
+      </ul>
+ 
+    
     </div>
   );
 }
